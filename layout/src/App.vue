477146777ce1efec3c5030275759dfd76aa528ed
JHXXRKMR <91938810+JHXXRKMR@users.noreply.github.com>
2021-10-18 10:38:33 +0800

    Layout
    Codes for the layout.

---
+++ layout/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ <responsive/>
+</template>
+
+<script>
+import responsive from './components/responsive.vue'
+
+export default {
+  name: 'App',
+  components: {
+    responsive
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
