aaf2ceb981d127877e1ce3d08823d07b2f879002
hytangs <64950573+hytangs@users.noreply.github.com>
2021-10-19 15:32:07 +0800

    Adding & localizing admin plugin

---
+++ src/views/admin/AdminTemplate.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav-bar/>
+  <aside-menu :menu="menu"/>
+  <title-bar :title-stack="titleStack" />
+  <hero-bar>Blank Page</hero-bar>
+  <main-section>
+    <br> <br> <h1>    Blank Page for construction</h1> <br> <br>
+  </main-section>
+  <footer-bar/>
+  <overlay v-show="isAsideLgActive" z-index="z-30" @overlay-click="overlayClick" />
+</template>
+
+<script>
+import menu from '../../../src/views/admin/menu.js'
+import NavBar from '../../../src/components/plugins/NavBar'
+import AsideMenu from '../../../src/components/plugins/AsideMenu'
+import FooterBar from '../../../src/components/plugins/FooterBar'
+import Overlay from '../../../src/components/plugins/Overlay'
+import TitleBar from '../../../src/components/plugins/TitleBar'
+import HeroBar from '../../../src/components/plugins/HeroBar'
+import {ref} from "vue";
+
+export default {
+  name: "AdminTemplate",
+  components: {
+    Overlay,
+    FooterBar,
+    AsideMenu,
+    NavBar,
+    TitleBar,
+    HeroBar
+  },
+  setup() {
+    const titleStack = ref(['Admin', 'Blank Page'])
+
+    return {
+      titleStack,
+      menu,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
