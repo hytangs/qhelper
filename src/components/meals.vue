12a68d66044372d893c0282c0fedfc7edc1c6a24
enqiilim <65434451+enqiilim@users.noreply.github.com>
2021-10-21 23:02:42 +0800

    Update meals.vue

---
+++ src/components/meals.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class= meals>
+        <form>
+            <div class="mealForm">
+                <h1>Menu for 31st October, 2021</h1>
+                <h5>Choose 1 main, 3 side(s)</h5>
+            </div>
+            <div class="mainChoice">
+                <h2>Choose your main</h2>
+                <input type = "radio" id= "food1" name="main">
+                <label for="food1">Food 1</label>
+                <img src="https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" width="5%">
+                <input type="radio" id="food2" name="main">
+                <label for="food2">Food 2</label>
+                <!--<img src="https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale" width="5%">-->
+            </div>
+            <div class="sideChoice">
+                <h2>Choose 3 sides</h2>
+                <input type="checkbox" id="side1" name="side">
+                <label for="side1">Side 1</label><br>
+                <input type="checkbox" id="side2" name="side">
+                <label for="side2">Side 2</label><br>
+                <input type="checkbox" id="side3" name="side">
+                <label for="side3">Side 3</label><br>
+                <input type="checkbox" id="side4" name="side">
+                <label for="side4">Side 4</label><br>
+                <input type="checkbox" id="side5" name="side">
+                <label for="side5">Side 5</label><br>
+                <input type="checkbox" id="side6" name="side">
+                <label for="side6">Side 6</label><br>
+            </div>
+            <div class="submit">
+                <button id="submission" type ="button">Submit</button>
+            </div>
+        </form>
+        <h2>SHOW UP PLS</h2>
+    </div>    
+</template> 
+
+<style scoped>
+
+</style>
+
+<script>
+console.log("in here")
+export default {
+    name:'meals'
+        
+}
+</script>
