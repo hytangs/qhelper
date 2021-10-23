3de2b1bdffd43cd0fdcfbba9c2d9455f3e1a874e
VeronaChua <91944689+VeronaChua@users.noreply.github.com>
2021-10-24 01:21:52 +0800

    update design on guest service

--- src/views/admin/GuestService.vue
+++ src/views/admin/GuestService.vue
@@ -1,4 +1,9 @@
 <template>
+
+<modal-box v-model="isModalActive" title="Add New Room Types">
+  <p>Add <b>new room types</b></p>
+</modal-box>
+
   <nav-bar/>
   <aside-menu :menu="menu"/>
   <title-bar :title-stack="titleStack" />
@@ -8,18 +13,20 @@
     <div class="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3 ">
       <div class="col-span-2">
         <card-component class="mb-6" title="Room Status" has-table>
-          <field>
-            <control placeholder="Room Number" v-model="roomNumber" />
-          </field>
-          <field>
-            <control placeholder="Room Status" :options="roomStatusOptions" v-model="roomNumber" />
-          </field>
-          <field>
-            <control placeholder="Room Type" :options="roomTypeOptions" v-model="roomType" />
-          </field>
-          <jb-buttons type="justify-start lg:justify-end" no-wrap>
-            <jb-button color="info" label="Search" v-on:click="search()"/>
-          </jb-buttons>
+          <div class="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-4 ">
+            <field>
+              <control placeholder="Room Number" v-model="roomNumber" />
+            </field>
+            <field>
+              <control :options="roomStatusOptions" v-model="roomNumber" />
+            </field>
+            <field>
+              <control :options="roomTypeOptions" v-model="roomType" />
+            </field>
+            <jb-buttons type="justify-center lg:justify-center" no-wrap>
+              <jb-button color="info" label="Search" v-on:click="search()"/>
+            </jb-buttons>
+          </div>
           <GuestRoomStatus/>
         </card-component>
       </div>
@@ -27,7 +34,7 @@
       <card-component class="mb-6" title="Room Types & Rates" has-table>
         <GuestRoomType/>
         <jb-buttons type="justify-start lg:justify-end" no-wrap>
-          <jb-button color="info" label="Add New Room Types" v-on:click="saveTofs()"/>
+          <jb-button color="info" label="Add New Room Types" v-on:click="isModalActive = true, addNewRoom()"/>
         </jb-buttons>
       </card-component>
   </div>
@@ -52,6 +59,11 @@ import NavBar from '../../../src/components/plugins/NavBar'
 import AsideMenu from '../../../src/components/plugins/AsideMenu'
 import FooterBar from '../../../src/components/plugins/FooterBar'
 import Overlay from '../../../src/components/plugins/Overlay'
+import Field from '../../components/plugins/Field'
+import Control from '../../components/plugins/Control'
+import JbButton from '../../components/plugins/JbButton'
+import JbButtons from '../../components/plugins/JbButtons'
+import ModalBox from '../../components/plugins/ModalBox'
 import TitleBar from '../../../src/components/plugins/TitleBar'
 import HeroBar from '../../../src/components/plugins/HeroBar'
 import CardComponent from "../../components/plugins/CardComponent"
@@ -68,6 +80,11 @@ export default {
     FooterBar,
     AsideMenu,
     NavBar,
+    Field,
+    Control,
+    ModalBox,
+    JbButton,
+    JbButtons,
     TitleBar,
     HeroBar,
     CardComponent,
@@ -79,9 +96,37 @@ export default {
   setup() {
     const titleStack = ref(['Admin', 'Rooms'])
 
+    const isModalActive = ref(false)
+
+    const roomStatusOptions = [
+      "Occupied",
+      "Vacant",
+      "Cleaning in progress"
+    ]
+
+    const roomTypeOptions = [
+      "Single",
+      "Queen",
+      "King",
+      "Apartment"
+    ]
+
     return {
       titleStack,
+      isModalActive,
       menu,
+      roomStatusOptions,
+      roomTypeOptions
+    }
+  },
+
+  methods: {
+    async search() {
+      // search the room and display
+    },
+
+    async addNewRoom() {
+      // add new room
     }
   }
 }
