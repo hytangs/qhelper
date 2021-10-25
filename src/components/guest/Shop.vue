106c29d0405450bac0ee8484ed2b9a2c86e03125
enqiilim <65434451+enqiilim@users.noreply.github.com>
2021-10-25 14:34:51 +0800

    Added Shop.vue

---
+++ src/components/guest/Shop.vue
@@ -0,0 +1,23 @@
+<template>
+<div class = "shop-page">
+<NavBar2/><br>
+
+</div>
+</template>
+
+<script>
+import NavBar2 from './Shops/NavBar2'
+
+export default {
+    name: "Shops",
+    components:{
+        NavBar2
+    }
+}
+</script>
+
+<style scoped>
+.entertain{
+    width: 100%;
+}
+</style>
