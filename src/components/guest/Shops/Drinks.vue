015ab4bdf53fe4c56f675c5c00ae5d2c702becb4
enqiilim <65434451+enqiilim@users.noreply.github.com>
2021-10-26 11:31:50 +0800

    Update Drinks.vue

--- src/components/guest/Shops/Drinks.vue
+++ src/components/guest/Shops/Drinks.vue
@@ -1,5 +1,193 @@
 <template>
     <div id=drinks>
-        <h1>Drinks</h1>
+        <div class="grid grid-cols-2 gap-6 lg:grid-cols-3 mb-6">
+                <div class="bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-900 md:rounded shadow">
+                    <div class="item_img">
+                        <img src="https://www.kuali.com/wp-content/uploads/2016/06/Teh-Tarik-stock-photo.jpg">
+                    </div>    
+                    <div style="margin:15px 30px 15px 30px " class="item_info">
+                        <h4 style="float: left">Teh Tarik</h4>
+                        <h4 style="float: right">$3.00</h4>
+                        <br><br>
+                        <div class="flex-none w-full grid gap-3 text-center" style="grid-template-columns: 1fr 1fr auto;">
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(0,0,0); border-color: rgb(0, 0, 0); transform-origin: 0% 100% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-white font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 50% 50% 0px;">Buy now</span>
+                                    </span>
+                                </div>
+                            </button>
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(255, 255, 255); border-color: rgb(212, 212, 216); transform-origin: 50% 50% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-black font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 0% 50% 0px;">Add to Cart</span>
+                                    </span>
+                                    
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-900 md:rounded shadow">
+                    <div class="item_img">
+                        <img src="https://media.npr.org/assets/img/2015/11/19/salt-iconic-branding-0037edit_custom-106133bb015b56c78e6bd554cfcbc9cc648156bb.jpg">
+                    </div>    
+                    <div style="margin:15px 30px 15px 30px " class="item_info">
+                        <h4 style="float: left">Coca Cola</h4>
+                        <h4 style="float: right">$5.00</h4>
+                        <br><br>
+                        <div class="flex-none w-full grid gap-3 text-center" style="grid-template-columns: 1fr 1fr auto;">
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(0,0,0); border-color: rgb(0, 0, 0); transform-origin: 0% 100% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-white font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 50% 50% 0px;">Buy now</span>
+                                    </span>
+                                </div>
+                            </button>
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(255, 255, 255); border-color: rgb(212, 212, 216); transform-origin: 50% 50% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-black font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 0% 50% 0px;">Add to Cart</span>
+                                    </span>
+                                    
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div><div class="bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-900 md:rounded shadow">
+                    <div class="item_img">
+                        <img src="http://cdn.shopify.com/s/files/1/1057/7864/files/blog_shutterstock_460627045.jpg?4410922092542058212">
+                    </div>    
+                    <div style="margin:15px 30px 15px 30px " class="item_info">
+                        <h4 style="float: left">Soya Milk</h4>
+                        <h4 style="float: right">$3.50</h4>
+                        <br><br>
+                        <div class="flex-none w-full grid gap-3 text-center" style="grid-template-columns: 1fr 1fr auto;">
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(0,0,0); border-color: rgb(0, 0, 0); transform-origin: 0% 100% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-white font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 50% 50% 0px;">Buy now</span>
+                                    </span>
+                                </div>
+                            </button>
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(255, 255, 255); border-color: rgb(212, 212, 216); transform-origin: 50% 50% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-black font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 0% 50% 0px;">Add to Cart</span>
+                                    </span>
+                                    
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div><div class="bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-900 md:rounded shadow">
+                    <div class="item_img">
+                        <img src="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5e325c56f133f400076b17b9%2F0x0.jpg">
+                    </div>    
+                    <div style="margin:15px 30px 15px 30px " class="item_info">
+                        <h4 style="float: left">Corona Beer</h4>
+                        <h4 style="float: right">$8.00</h4>
+                        <br><br>
+                        <div class="flex-none w-full grid gap-3 text-center" style="grid-template-columns: 1fr 1fr auto;">
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(0,0,0); border-color: rgb(0, 0, 0); transform-origin: 0% 100% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-white font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 50% 50% 0px;">Buy now</span>
+                                    </span>
+                                </div>
+                            </button>
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(255, 255, 255); border-color: rgb(212, 212, 216); transform-origin: 50% 50% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-black font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 0% 50% 0px;">Add to Cart</span>
+                                    </span>
+                                    
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div><div class="bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-900 md:rounded shadow">
+                    <div class="item_img">
+                        <img src="https://res.cloudinary.com/enotria/image/upload/ar_16:9,c_fill,dpr_3.0,w_600/v1615237634/Singpore_S_Ling_banner_8dbba080d7.jpg">
+                    </div>    
+                    <div style="margin:15px 30px 15px 30px " class="item_info">
+                        <h4 style="float: left">Singapore Sling</h4>
+                        <h4 style="float: right">$10.00</h4>
+                        <br><br>
+                        <div class="flex-none w-full grid gap-3 text-center" style="grid-template-columns: 1fr 1fr auto;">
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(0,0,0); border-color: rgb(0, 0, 0); transform-origin: 0% 100% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-white font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 50% 50% 0px;">Buy now</span>
+                                    </span>
+                                </div>
+                            </button>
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(255, 255, 255); border-color: rgb(212, 212, 216); transform-origin: 50% 50% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-black font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 0% 50% 0px;">Add to Cart</span>
+                                    </span>
+                                    
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div><div class="bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-900 md:rounded shadow">
+                    <div class="item_img">
+                        <img src="https://d2lswn7b0fl4u2.cloudfront.net/photos/pg-2-bubble-tea-1608310012.jpg">
+                    </div>    
+                    <div style="margin:15px 30px 15px 30px " class="item_info">
+                        <h4 style="float: left">Milk Tea with Pearls</h4>
+                        <h4 style="float: right">$5.00</h4>
+                        <br><br>
+                        <div class="flex-none w-full grid gap-3 text-center" style="grid-template-columns: 1fr 1fr auto;">
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(0,0,0); border-color: rgb(0, 0, 0); transform-origin: 0% 100% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-white font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 50% 50% 0px;">Buy now</span>
+                                    </span>
+                                </div>
+                            </button>
+                            <button class="relative">
+                                <div class="absolute bg-black inset-px" style="transform-origin: 0% 0% 0px; border-radius: 4.44444% / 16.6666%;"></div>
+                                <div class="relative text-sm border cursor-pointer" style="height: 38px; background-color: rgb(255, 255, 255); border-color: rgb(212, 212, 216); transform-origin: 50% 50% 0px; border-radius: 4.37956% / 15.7895%;">
+                                    <span class="absolute inset-0 flex items-center justify-center text-black font-medium" style="opacity: 1;">
+                                        <span style="transform-origin: 0% 50% 0px;">Add to Cart</span>
+                                    </span>
+                                    
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.item_info{
+    display: inline-flex,
+}
+
+h4{
+    font-size: 20px
+}
+
+img {
+    text-align: center;
+    display: block;
+    width: 100%;
+    height: 200px;
+}
+</style>
