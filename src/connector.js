922e32fb84ba1eb557950ae6a23495cd59d3e133
hytangs <64950573+hytangs@users.noreply.github.com>
2021-10-31 03:02:15 +0800

    Security Update: using sha256 to hash admin password

--- src/connector.js
+++ src/connector.js
@@ -1,6 +1,7 @@
 
 import firebaseApp from "./firebase";
 import {getFirestore, doc, getDoc} from "firebase/firestore";
+import sha256 from "./components/plugins/helpers/sha256"
 
 const db = getFirestore(firebaseApp);
 
@@ -10,10 +11,11 @@ export default {
         async checkPwd(inAccount, inPassword) {
             const docRef = doc(db, "AdminAccount", String(inAccount));
             let docSnap = await getDoc(docRef);
-
+            console.log(sha256)
+            inPassword = sha256(inPassword)
             if (docSnap.exists()) {
-                console.log("Document data:", docSnap.data().password);
-                return docSnap.data().password === inPassword;
+                console.log("Document data:", docSnap.data().passwordhash);
+                return docSnap.data().passwordhash === inPassword;
             } else {
                 // doc.data() will be undefined in this case
                 console.log("No such document!");
