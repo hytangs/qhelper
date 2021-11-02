bfe02a8938f7380944a71a7e661bfa72daad3726
hytangs <64950573+hytangs@users.noreply.github.com>
2021-11-02 16:50:58 +0800

    solving

---
+++ src/components/admin/admin-components/GuestEntry.vue
@@ -0,0 +1,33 @@
+<template>
+  <tr>
+    <td data-label="Room Number"> {{ RoomNumber }} </td>
+    <td data-label="Status"> {{ Status }} </td>
+    <td data-label="Type"> {{ RoomType }} </td>
+    <td data-label="Check Out"> {{ Checkout }} </td>
+    <td data-label="Requests"> {{ Requests }} </td>
+    <td class="actions-cell">
+      <jb-buttons type="justify-start lg:justify-end" no-wrap>
+        <jb-button class="mr-3" color="light" :icon="mdiPencilOutline" small @click="isModalActive = true" />
+      </jb-buttons>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "GuestEntry",
+
+  props: {
+    RoomNumber: String,
+    Status: String,
+    RoomType: String,
+    Checkout: String,
+    Requests: String
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
