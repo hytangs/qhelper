d6c937a94f0da342aad9364d39f1718461d1ddc8
hytangs <64950573+hytangs@users.noreply.github.com>
2021-11-04 18:11:28 +0800

    Major: Zone Access Control

--- src/views/admin/HealthDeclaration.vue
+++ src/views/admin/HealthDeclaration.vue
@@ -3,7 +3,7 @@
   <aside-menu :menu="menu"/>
   <title-bar :title-stack="titleStack" />
   <hero-bar v-if="zone === '1' || zone === '2'">Quarantine Settings</hero-bar>
-  <hero-bar v-else>Unauthorized</hero-bar>
+  <hero-bar v-else>Unauthorized - Please contact web administrator.</hero-bar>
   <main-section  v-if="zone === '1' || zone === '2'">
     <br>
     <card-component class="mb-6" title="Quarantine Status" has-table>
@@ -35,17 +35,12 @@
     </card-component>
 
   </main-section>
-  <main-section v-else>
-    <h1 class="text-2xl text-gray-700 hover:text-gray-900">
-      You don't have access to visit this page.<br />
-      Please contact web administrator for more info.
-    </h1>
-  </main-section>
-  <footer-bar/>
+  <footer-bar v-if="zone === '1' || zone === '2'"/>
   <overlay v-show="isAsideLgActive" z-index="z-30" @overlay-click="overlayClick" />
 </template>
 
 <script>
+import { useStore } from "vuex";
 import menu from './menu.js'
 import NavBar from '../../../src/components/plugins/NavBar'
 import AsideMenu from '../../../src/components/plugins/AsideMenu'
@@ -59,7 +54,7 @@ import QuarantinePlan from '../../../src/components/admin/admin-components/Quara
 import QuarantineHealthAlert from '../../../src/components/admin/admin-components/QuarantineHealthAlert'
 import GuestRoomBroadcast from '../../components/admin/admin-components/PublicBroadcast'
 import QuarantineMedicalCheckouts from '../../../src/components/admin/admin-components/QuarantineMedicalCheckouts'
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import connector from "../../connector"
 import localsession from "../../store/localsession";
 
@@ -82,6 +77,10 @@ export default {
   setup() {
     const titleStack = ref(['Admin', 'Health'])
 
+    const store = useStore()
+
+    const darkMode = computed(() => store.state.darkMode);
+
     const zone = localsession.methods.getAdminZone();
 
     connector.methods.getHealthAlert()
@@ -89,7 +88,8 @@ export default {
     return {
       titleStack,
       menu,
-      zone
+      zone,
+      darkMode
     }
   }
 }
